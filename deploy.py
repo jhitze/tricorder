b163b57a40eead5e577ef3949dd9b9b7166a61e9
Jacob Hitze <jedmaster@gmail.com>
2022-08-02 23:10:19 -0400

    update deploy to make folders

--- deploy.py
+++ deploy.py
@@ -1,10 +1,10 @@
 import glob
 import shutil
 import os
+from pathlib import Path
 
 files = [
-    "code.py",
-    "scd30.py"
+    "code.py"
 ]
 
 folders = [
@@ -20,6 +20,11 @@ destination = os.path.abspath(destination)
 def deploy(file_name):
     source = os.path.join(path, file_name)
     final_destination = os.path.join(destination, file_name)
+    # os.makedirs(final_destination, exist_ok=True)
+    file_directory = os.path.dirname(final_destination)
+    if(not os.path.exists(file_directory)):
+        print("Making directory: {}".format(file_directory))
+        os.makedirs(file_directory, exist_ok=True)
     print("Copying {} to {}".format(source, final_destination))
     shutil.copy(source, final_destination)
 
