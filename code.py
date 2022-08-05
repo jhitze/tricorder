e6e36b76fd9122dbbdb246183e148a43e70f1b49
Jacob Hitze <jedmaster@gmail.com>
2022-08-05 01:47:33 -0400

    removed being shiny for no reason

--- code.py
+++ code.py
@@ -1,18 +1,15 @@
-import time
 import board
 import neopixel
 from pages.pages import Pages
 import touchio
 import digitalio
 import asyncio
-from pages import BLACK
 
 
 i2c = board.I2C()
 display = board.DISPLAY
 pixel_pin = board.NEOPIXEL
 num_pixels = 4
-
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=False)
 
 # Perform a couple extra steps for the HalloWing M4
@@ -29,43 +26,6 @@ touch_A2 = touchio.TouchIn(board.TOUCH1)
 touch_A3 = touchio.TouchIn(board.TOUCH2)
 touch_A4 = touchio.TouchIn(board.TOUCH3)
 touch_A5 = touchio.TouchIn(board.TOUCH4)
-
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        return (0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3)
-
-
-def color_chase(color, wait):
-    for i in range(num_pixels):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait)
-
-
-max_co2 = 0
-
-# Do something to show that it's loading.
-rainbow_cycle(0)
-
-
-color_chase(BLACK, 0.01)
 
 async def user_input_checker(pages):
     while True:
